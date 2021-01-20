6c860fb842a2d109659211af3247d60d00b4c560
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2021-01-20 12:22:39 -0300

    Inserindo ação do botão de remover produto

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -70,6 +70,12 @@ class ProductController extends Controller
 
     public function destroy($product)
     {
-        return $product;
+        $product = $this->product->find($product);
+
+        $product->delete();
+
+        flash('Produto removido com sucesso!')->success()->important();
+
+        return redirect()->route('products.index');
     }
 }
