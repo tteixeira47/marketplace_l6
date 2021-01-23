52e6023e83dea7f2b035c507ddc7e0de07db149d
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2021-01-23 05:08:27 -0300

    Ajustando models para gerar slug

--- app/Category.php
+++ app/Category.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    use HasSlug;
+
     protected $fillable = ['name', 'description', 'slug'];
 
     public function products()
--- app/Store.php
+++ app/Store.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
+    use HasSlug;
+
     protected $fillable = ['name','description', 'phone', 'mobile_phone', 'slug'];
     
     public function user()
@@ -16,5 +20,12 @@ class Store extends Model
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
