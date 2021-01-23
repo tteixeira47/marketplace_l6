7350d8d66a4db766e589398eaab1d0a73bb5babc
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2021-01-23 01:49:23 -0300

    Criando CategoryController e CategoryRequest

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,82 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    private $category;
+
+	public function __construct(Category $category)
+	{
+		$this->category = $category;
+	}
+
     public function index()
     {
-        //
+        $categories = $this->category->paginate(10);
+
+        return view('admin.categories.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('admin.categories.create');    
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $category = $this->category->create($data);
+
+        flash('Categoria criada com Sucesso!')->success()->important();
+        return redirect()->route('admin.categories.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show($category)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit($category)
     {
-        //
+        $category = $this->category->findOrFail($category);
+
+        return view('admin.categories.edit', compact('category'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(CategoryRequest $request, $category)
     {
-        //
+        $data = $request->all();
+
+        $category = $this->category->find($category);
+        $category->update($data);
+
+        flash('Categoria atualizada com Sucesso!')->success()->important();
+        return redirect()->route('admin.categories.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $category = $this->category->find($category);
+	    $category->delete();
+
+	    flash('Categoria Removida com Sucesso!')->success();
+	    return redirect()->route('admin.categories.index');
     }
 }
