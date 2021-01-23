98474c4d1da93e7db637088b613615fc4df3395c
Thiago Teixeira <thiagoh.teixeira@outlook.com>
2021-01-23 01:51:13 -0300

    Ajustando CategoryRequest

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -13,7 +13,7 @@ class CategoryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,15 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'required' => 'Este campo é obrigatório',
+    ];
+    }
+
 }
